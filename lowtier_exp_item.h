66a9799fa82223c61e3d2d3ced15b13ef57561c9
Riugo-dev <riugohonda.hal@gmail.com>
2025-09-01 16:16:20 +0900

    一旦型作り

---
+++ lowtier_exp_item.h
@@ -0,0 +1,20 @@
+//********************************************************************************
+//
+// lowtier_exp_item.h[’e]
+//
+//															Author :Riugo Honda
+//															Date   :2025/09/01
+//********************************************************************************
+
+#ifndef _LOWTIER_EXP_ITEM
+#define _LOWTIER_EXP_ITEM
+
+#include "exp_item.h"
+
+class LowTierExpItem : public ExpItem
+{
+
+};
+
+
+#endif // !_LOWTIER_EXP_ITEM
