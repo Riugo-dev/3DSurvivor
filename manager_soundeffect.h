4b0d31e012203f776248da8587273835c251f883
Riugo-dev <riugohonda.hal@gmail.com>
2025-09-19 13:14:58 +0900

    audio関連を少し整理

--- manager_soundeffect.h
+++ manager_soundeffect.h
@@ -12,24 +12,25 @@
 #ifndef _SOUNDEFFECT_MANAGER_
 #define _SOUNDEFFECT_MANAGER_
 
-#include <vector>
+#include <unordered_map>
+#include <memory>
 
 class Audio;
 
 typedef enum
 {
 	SE_NONE = -1,
-	SE_EXPLOSION,
-	SE_SHOT,
-	SE_PARTICLEGATHER,
-	SE_SPAWN,
-	SE_COINGET,
+	SE_BULLETSHOT,
+	SE_SWORDTHROW,
+	SE_SHURIKENTHROW,
+	SE_ENEMYDAMAGE,
+	SE_NEWRECORD,
 }SoundEffect;
 
 class SoundEffectManager
 {
 private:
-	static std::vector<Audio*> m_SoundEffects;
+	static std::unordered_map<SoundEffect , std::unique_ptr<Audio>> m_pSoundEffects;
 
 public:
 	SoundEffectManager();
