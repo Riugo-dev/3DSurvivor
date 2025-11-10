01ac60c476931f5e5788b95f6d4f9899744aa771
Riugo-dev <riugohonda.hal@gmail.com>
2025-11-10 17:12:47 +0900

    memoを参考に作り替える

--- modelRenderer.cpp
+++ modelRenderer.cpp
@@ -1,10 +1,11 @@
+#include "main.h"
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <shlwapi.h>
 #pragma comment(lib, "shlwapi.lib")
 
-#include "main.h"
+
 #include "renderer.h"
 #include "modelRenderer.h"
 
