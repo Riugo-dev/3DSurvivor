833671ef96ddf4ec8fd2d213c7fbcf52c8331121
Riugo-dev <riugohonda.hal@gmail.com>
2026-02-25 16:18:06 +0900

    一旦保留

---
+++ enemy_gpu_manager.h
@@ -0,0 +1,22 @@
+//********************************************************************************
+//
+// enemy_gpu_manager.h[ìGGPUä«óùÉNÉâÉX]
+//
+//															Author :Riugo Honda
+//															Date   :2026/02/25
+//********************************************************************************
+
+#ifndef _ENEMY_GPU_MANAGER_H_
+#define _ENEMY_GPU_MANAGER_H_
+
+class GPUEnemyManager
+{
+private:
+	static GPUEnemyManager* m_pMyself;
+
+
+
+
+};
+
+#endif // !_ENEMY_GPU_MANAGER_H_
